5ee64d7f1151fbcb7652a35b49703d62e8c8ea34
neto <euneto2501@gmail.com>
2022-10-25 03:15:41 -0300

    feature: atualizacao no banco de imagens

--- braille.m
+++ braille.m
@@ -3,17 +3,32 @@ clear all
 
 pkg load image
 
-im = imread('C:\Users\eucli\Documents\Projetos\Matlab\PDI_Braille\banco_de_imagens\braille_y.jpeg');
+im = imread('C:\Users\eucli\Documents\Projetos\Matlab\PDI_Braille\banco_de_imagens\braille_o_rolha.jpeg');
 
 figure('Name','Imagem original');
-imshow(im)
+imshow(im);
 
-imGray = rgb2gray(im);
+imSemVerde = im;
+
+for(i=1:1:size(im,1)) 
+  for(j=1:1:size(im,2))
+    if(im(i,j,1)> 100 && im(i,j,2) > 100 && im(i,j,3) < 100)
+      imSemVerde(i,j, 1) = 255;
+      imSemVerde(i,j, 2) = 0;
+      imSemVerde(i,j, 3) = 0;
+    endif
+  endfor
+endfor
+
+figure('Name','Imagem sem verde');
+imshow(imSemVerde);
+
+imCinza = rgb2gray(imSemVerde);
 figure('Name','Imagem original em intensidades de cinza');
-imshow(imGray)
+imshow(imCinza);
 
-figure('Name','Histograma original');
-imhist(imGray)
+figure('Name','Histograma');
+imhist(imCinza);
 
 pontoDeCorte = 100;
 
@@ -21,13 +36,13 @@ im2 = uint8(zeros(size(im,1), size(im,2)));
 
 for(i=1:1:size(im2,1)) 
   for(j=1:1:size(im2,2))
-    if(im(i,j) > pontoDeCorte)
+    if(imCinza(i,j) > pontoDeCorte)
       im2(i,j,:) = 255;
     endif
   endfor
 endfor
 
-figure(4);
+figure('Name', 'Máscara');
 imshow(im2);
 
 imfinal = im;
@@ -38,6 +53,6 @@ for(i=1:size(im,1))
     endif
   endfor
 endfor
-figure('Name','Grãos segmentados automaticamente')
-imshow(imfinal)
+figure('Name','Imagem final');
+imshow(imfinal);
 
